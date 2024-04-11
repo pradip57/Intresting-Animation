93b82c18d1481fe95babc98ab738c16c1cecf6a8
Pradip <dellpidarp57@gmail.com>
2024-04-11 17:54:57 +0545

    first animation with html,css and js

---
+++ script.js
@@ -0,0 +1,8 @@
+function animation1(){
+
+    document.getElementById("stand1").style.animation = "round 6s linear infinite";
+}
+
+function animation2(){
+    document.getElementById("ball").style.animation = "ballmove 3s linear  infinite alternate 3.25s";
+}
